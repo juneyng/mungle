f5543e83fb5e2c8976a9f55f78abc6264252d45f
JunYoung Lee <120372886+juneyng@users.noreply.github.com>
2025-07-15 10:41:30 +0900

    feat[#1]: 프로젝트 생성 및 예시 데이터 추가

---
+++ src/main/java/com/juneyng/mungle/MungleApplication.java
@@ -0,0 +1,13 @@
+package com.juneyng.mungle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MungleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MungleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juneyng/mungle/MungleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juneyng.mungle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MungleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
